b73e673cd9def1983357927677e36fda40c465a4
Roznatovskyi Vladyslav <morphey138@gmail.com>
2023-12-09 20:52:49 +0200

    function changes

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-module.exports = function findMostUsedLetter(text) {
-    // Convert the text to lowercase to make the search case-insensitive
+module.exports = function mostUsedLetter(text) {
     const lowercaseText = text.toLowerCase();
   
-    // Create an object to store the frequency of each letter
     const letterFrequency = {};
   
-    // Iterate through each character in the text
     for (let i = 0; i < lowercaseText.length; i++) {
       const char = lowercaseText[i];
   
-      // Check if the character is a letter
       if (/[a-z]/.test(char)) {
-        // Increment the frequency count for the letter in the object
         letterFrequency[char] = (letterFrequency[char] || 0) + 1;
       }
     }
   
-    // Find the letter with the highest frequency
     let mostUsedLetter;
     let highestFrequency = 0;
   
     for (const letter in letterFrequency) {
+      if (letterFrequency[letter] > highestFrequency) {
         mostUsedLetter = letter;
         highestFrequency = letterFrequency[letter];
       }
@@ -28,8 +23,3 @@ module.exports = function findMostUsedLetter(text) {
   
     return mostUsedLetter;
   }
-  
-  // Example usage
-  const text = "This is a sample text.";
-  const mostUsed = findMostUsedLetter(text);
-  console.log(`The most used letter is: ${mostUsed}`);
